19d6f5b23de303645a249bb6962df49d47858e12
felyRico <emanresutresni@gmail.com>
2024-11-12 11:29:33 +0700

    Add folder-name with initial files

---
+++ react-1/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <header>
+      <h1>Rico's Anecdote</h1>
+    </header>
+      <header>
+        
+      </header>
+    </>
+  );
+}
+
+export default App;
